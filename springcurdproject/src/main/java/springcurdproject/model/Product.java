e7c4bb05e53761cccfe1a25cde4d25e3b9efb544
nishant <nsngh96@gmail.com>
2023-04-23 20:39:02 +0530

    spring mvc curd jsp using hibernate

---
+++ springcurdproject/src/main/java/springcurdproject/model/Product.java
@@ -0,0 +1,64 @@
+package springcurdproject.model;
+
+
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	private String email;
+	private int age;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Product(int id, String name, String email, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.age = age;
+	}
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", email=" + email + ", age=" + age + "]";
+	}
+	
+	
+	
+	
+}
